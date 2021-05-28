66e71bc5fb4dc7fcebb6eeb899b277f3b08208be
jay bhavsar <jay.bhavsar@laitmatus.com>
2021-05-28 13:49:31 +0530

    fiexed untracked files

--- back/src/main/scala/com/example/MathRegistry.scala
+++ back/src/main/scala/com/example/MathRegistry.scala
@@ -1,5 +1,21 @@
 package com.example
 
-class MathRegistry {
+import akka.actor.typed.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import scala.collection.immutable
+
+
+//refere this linke for formulas.
+//https://www.calculatorsoup.com/calculators/math/percentage.php
+final case class Ppercent(percent: Double, amount: Double)
+final case class WhatPercent(amount1: Double, amount2: Double)
+final case class WhatAmount(amount: Double, percent: Double)
+
+
+object MathRegistry {
+
+  sealed trait Command
+  final case class DoPpercent()
 
 }
