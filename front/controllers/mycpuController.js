3036d8cbd2d92e5824c945f1f560663c79af03c0
jay bhavsar <jay.bhavsar@laitmatus.com>
2021-08-05 12:36:41 +0530

    stream fetching done.

--- front/controllers/mycpuController.js
+++ front/controllers/mycpuController.js
@@ -2,38 +2,72 @@ app.controller('mycpuController', function($scope, $http){
 	$scope.val = "mycpuController" 
 
 
-    // google.charts.load('current', {'packages':['corechart']});
-    //   google.charts.setOnLoadCallback(drawChart);
+    $scope.mygraph = {
+        columns: ['', 'Usage', 'Swap Memory'],
+        data: [['', 'Usage', 'Swap Memory']]
+    }
 
-    //   function drawChart() {
-    //     var data = google.visualization.arrayToDataTable([
-    //       ['Year', 'Sales', 'Expenses'],
-    //       ['2004',  1000,      400],
-    //       ['2005',  1170,      460],
-    //       ['2006',  660,       1120],
-    //       ['2007',  1030,      540]
-    //     ]);
 
-    //     var options = {
-    //       title: 'Company Performance',
-    //       curveType: 'function',
-    //       legend: { position: 'bottom' }
-    //     };
 
-    //     var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    Array.prototype.enqueue = function(val){
+        if(this.length === this.queueLengthIs){
+            this.shift()
+            this.push(val)
+        }else{
+            this.push(val)
+        }
+        return this
+    }
+    Array.prototype.setQueueLength = function(val){
+        this.queueLengthIs = val
+    }
+    
+    let queue = new Array()
+    queue.setQueueLength(15)
+    
+    google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
-    //     chart.draw(data, options);
-    //   } 
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable($scope.mygraph.data);
 
-      $scope.doReq = function() {
-        $http.get("http://localhost:8181/events").then(
-			function successcallback(res) {
-				$scope.result = res; 
-			}, 
-			function errorcallback(res) {
-				$scope.result = res; 
-			}
-		)
-      }
+        var options = {
+          title: 'Live CPU Usage',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
 
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      } 
+
+        /**
+         * class in backend is as follows 
+         * case class Graph(cpuLoad: String, freeMem: String, freeSwap: String)
+         */
+
+
+
+
+        $scope.msg = {} 
+        var handlecallBack = function(msg) {
+            $scope.$apply(function() {
+                $scope.msg = JSON.parse(msg.data) 
+                
+                console.log('msg received', $scope.msg)
+                let c = parseInt($scope.msg.cpuLoad) 
+                let d = parseInt($scope.msg.freeMem)
+                // console.log(d)
+                // queue.enqueue(['', 'Sales', 'Expenses'])
+                queue.enqueue(['', c, d])
+                $scope.mygraph.data = [$scope.mygraph.columns, ...queue]
+                // console.log($scope.mygraph.data)
+                drawChart()
+            })
+        } 
+        var source = new EventSource('http://localhost:8181/events') 
+
+        source.addEventListener('message', handlecallBack, false)
+    
 })
